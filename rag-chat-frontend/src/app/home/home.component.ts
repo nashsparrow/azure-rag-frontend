471c11705cbd42b422b4a6c1f2092b1f418953f8
nashsparrow <fernandowwnm@gmail.com>
2026-06-28 23:22:55 +0200

    upload file tile implemented

--- rag-chat-frontend/src/app/home/home.component.ts
+++ rag-chat-frontend/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { ChatComponent } from '../chat/chat.component';
 import { ApiService } from '../services/api.service';
 import { DocumentListDialogComponent } from '../document-list-dialog/document-list-dialog.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +17,7 @@ import { DocumentListDialogComponent } from '../document-list-dialog/document-li
     MatButtonModule,
     ChatComponent,
     MatDialogModule,
+    CommonModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
@@ -55,9 +57,23 @@ export class HomeComponent {
   documents: any[] = [];
   private apiService = inject(ApiService);
   private dialog = inject(MatDialog);
+  selectedFile: File | null = null;
+  selectedFileName = '';
 
   constructor() {
     this.getAllDocuments();
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if (!file) {
+      return;
+    }
+
+    this.selectedFile = file;
+    this.selectedFileName = file.name;
   }
 
   getAllDocuments() {
